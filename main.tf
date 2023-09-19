f7a95d02444d30e6f69f56bafdb637dd016071b9
lalita2407 <lalitalallu02@gmail.com>
2023-09-19 13:44:09 +0530

    dummy

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+# Creates additional node pool
 resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
   name                  = var.name
   kubernetes_cluster_id = var.kubernetes_cluster_id
   vm_size               = var.vm_size
-  enable_auto_scaling   = true
+  enable_auto_scaling   = var.enable_auto_scaling
   os_disk_size_gb       = var.os_disk_size_gb
   workload_runtime      = "OCIContainer"
   zones                 = var.zones
@@ -11,11 +12,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
   max_count             = var.secondary_max_count
   max_pods              = var.secondary_max_pods
   os_sku                = var.os_sku
-  vnet_subnet_id        = var.subnet_id
+  vnet_subnet_id        = var.vnet_subnet_id
   priority              = var.priority
-  eviction_policy       = var.eviction_policy
-  # node_labels = var.node_labels
-  # node_taints = var.node_taints
+  node_labels = var.node_labels
+  node_taints = var.node_taints
   lifecycle {
     ignore_changes = [
       tags,
